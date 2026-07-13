35c098964b0e607a8a013a3540fc349be9f6413d
WONG-0718 <wongjw-wm24@student.tarc.edu.my>
2026-07-13 15:31:27 +0800

    Add files via upload

---
+++ Demo/Demo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers;
+
+// TODO
+public class HomeController : Controller
+{
+    // GET: Home/Index
+    public IActionResult Index()
+    {
+        // TODO
+
+        return View();
+    }
+
+    // GET: Home/Detail
+    public IActionResult Detail()
+    {
+        // TODO
+
+        return View();
+    }
+}
---
+++ Demo/Demo/Models/DB.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Models;
+
+#nullable disable warnings
+
+public class DB(DbContextOptions options) : DbContext(options)
+{
+    // DbSet
+    // TODO
+}
+
+// Entity Classes -------------------------------------------------------------
+
+public class Program
+{
+    // Column
+    // TODO
+
+    // Navigation
+    // TODO
+}
+
+public class Student
+{
+    // Column
+    // TODO
+
+    // FK
+    // TODO
+
+    // Navigation
+    // TODO
+}
