eb95d4a7ab7cc8b1a84550521da1d2aba13b0f41
WONG-0718 <wongjw-wm24@student.tarc.edu.my>
2026-07-13 15:40:59 +0800

    Add files via upload

---
+++ P5/Models/ViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models;
+
+#nullable disable warnings
+
+// View Models ----------------------------------------------------------------
+
+public class TeacherVM
+{
+    public string? Id { get; set; }
+
+    [StringLength(100)]
+    public string Name { get; set; }
+}
+
+public class SubjectVM
+{
+    public string? Id { get; set; }
+
+    [StringLength(100)]
+    public string Name { get; set; }
+}
